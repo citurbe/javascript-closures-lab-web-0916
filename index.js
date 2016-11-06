0cf334ca8b5693dca57277586f2c88555c60e243
Craig Iturbe <citurbe@gmail.com>
2016-11-06 13:56:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = "I don't do much."
+function bumpCounter() {
+  var counter = 0;
+  function addBump() {
+    counter++;
+  }
+  function getBumps() {
+    return counter;
+  }
+
+  return {addBump, getBumps}
+}
+
+function createAnimal(animalType) {
+  return function showtime(deadlyDevice) {
+    return {animalType:`${animalType}`, deadlyDevice:`${deadlyDevice}`}
+  }
+}
+
+const sharkCreator = createAnimal('Shark')
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
